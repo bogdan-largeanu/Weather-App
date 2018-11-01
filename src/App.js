7f81b16b19d06d4882653f0bfbe6ee320e31ffa2
Ionut Bogdan Largeanu <ilargeanu@and.digital>
2018-11-01 15:45:29 +0000

    add Weather app - 5 days api call

--- src/App.js
+++ src/App.js
@@ -1,28 +1,157 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import cloudSun from "./clouds-sun.png";
+import image from "./image.svg";
+import axios from "axios";
+import moment from "moment";
+import "./App.css";
+
+/*
+take api date of first entry and return a list with 
+  {hour: nameOfDay: date: }
+ for the next 5 days
+*/
+function matchDay(firstDayEntry) {
+  let daysOfWeek = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ];
+  let hour = moment(firstDayEntry).format("HH:mm:ss");
+  let dateInput = moment(firstDayEntry).format("dddd");
+  let index = daysOfWeek.indexOf(dateInput);
+  //make list of days the data is available for
+  let weatherDays = [];
+  for (let i = 0; i < 5; i++) {
+    let entry = {
+      nameOfDay: daysOfWeek[(index + i) % 7],
+      hour: hour
+    };
+    weatherDays.push(entry);
+  }
+  addDates(weatherDays, firstDayEntry);
+  return weatherDays;
+}
+/*
+Add to the list of object days, their matching calendar date
+*/
+
+function addDates(weatherDays, firstDayEntry) {
+  //To-DO: increase day by 1
+  let newDate;
+  let dateFormated;
+  let dateNow = moment().format("YYYY-MM-D");
+  let dateInput = firstDayEntry;
+  for (let i = 0; i < 5; i++) {
+    newDate = moment(dateInput).add(i, "d");
+    dateFormated = newDate.format("YYYY-MM-DD");
+    weatherDays[i] = { date: dateFormated, ...weatherDays[i] };
+  }
+
+  return weatherDays;
+}
+/*
+Add to the list of object days, their matching temperatures and descriptions
+*/
+function daysMaped(apiData, weatherDays) {
+  weatherDays.map(day => {
+    apiData.list.map(apiDay => {
+      const matchingDate = day.date + " " + day.hour;
+      if (apiDay.dt_txt === matchingDate) {
+        day.minTemp = Math.round(apiDay.main.temp_min-273.15);
+        day.maxTemp = Math.round(apiDay.main.temp_max-273.15);
+        day.description = apiDay.weather[0].description;
+      }
+    });
+  });
+
+  return weatherDays;
+}
+
+function Card(props) {
+  //it only comes here undefined
+  let day = [
+    {
+      date: "",
+      description: "",
+      hour: "",
+      maxTemp: 0,
+      minTemp: 0,
+      nameOfDay: ""
+    }
+  ];
+  if (typeof props.weather !== "undefined") {
+    day = props.weather;
+    console.log("Card(props)", day);
+  }
+  return (
+    <div className="flex-container">
+      <div className="day"> {day.nameOfDay}</div>
+      <div className="img">
+        {" "}
+        <img src={props.data.img} width="45px" />{" "}
+      </div>
+      <div className="flex-container-temp">
+        {" "}
+        {/* ?can I reuse css clases or is better to make a new one for each element?*/}
+        <div className="minTemp">{day.minTemp}</div>
+        <div className="maxTemp">{day.maxTemp}</div>
+      </div>
+      <div className="description">{day.description}</div>
+    </div>
+  );
+}
+
+function RowCards(props) {
+  const apiData = Object.assign({}, props.data.api);
+  let finalDataWeather =[{},{},{},{},{}];
+
+  if (typeof apiData.city != "undefined") {
+    //save date and match with day of the week
+    let weatherDays = matchDay(apiData.list[0].dt_txt);
+    //use same hour for each day to get data -> insdie call method to save variables for temperatures
+    finalDataWeather = daysMaped(apiData, weatherDays);
+  }
+  return (
+    <div className="flex-container-day">
+    {finalDataWeather.map( day=> <Card data={props.data} weather={day} />)}
+    </div>
+  );
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      min: 0,
+      max: 25,
+      day: "Mon",
+      img: cloudSun
+    };
+  }
+  componentDidMount() {
+    axios
+      .get(
+        "http://api.openweathermap.org/data/2.5/forecast?id=2643743&appid=b218ffa9d8261b193da591fdc8edb6e3"
+      )
+      .then(resp => {
+        this.setState(prevState => ({ api: resp.data }));
+      });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <RowCards data={this.state} />
       </div>
     );
   }
 }
 
+export { matchDay, daysMaped };
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-
+import App, { matchDay ,daysMaped} from './App';
+import api from './apiData.json'
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test ('Test matchDay is array ', ()=>
+{
+const list = matchDay('2018-10-31 18:00:00');
+expect(Array.isArray(list));
+} ) ;
+
+test ('Test MatchDay for fix-date and hour', ()=>{
+const day = matchDay('2018-10-31 18:00:00');
+expect(day[0].date).toBe('2018-10-31');
+});
+
+test ('Test MatchDay for invalid date', ()=>{
+  const day = matchDay('18:00:00');
+  expect(day[0].date).toBe('Invalid date');
+  });
+
+test('Test for correct 5th day fix-date only', ()=> {
+  const day = matchDay('2018-10-31');
+  expect(day[4].date).toBe('2018-11-04')
+});
+
+test('Test for correct 5th day fix-date only', ()=> {
+  const day = matchDay('2018-10-31 18:00:00');
+  expect(day[4].date).toBe('2018-11-04')
+});
+
+test('Test matching api object to my week object', ()=> {
+  //api.apiData.list[0].dt_txt
+  //api.apiData.list[1].main.temp_min
+  //api.apiData.list[1].weather[0].description
+  const weekData = daysMaped(api.apiData,api.listOfDays);
+  expect(weekData[0].nameOfDay).toBe(api.listOfDays[0].nameOfDay);
+});
+
+test('Test minimum temperature', ()=>{
+  const weekData = daysMaped(api.apiData, api.listOfDays);
+ //needs to be list 2 because that one matches with the correct hour & convert tmp
+ const tmpApi = Math.round(api.apiData.list[2].main.temp_min-273.15);
+  expect(weekData[0].minTemp).toBe(tmpApi)
+})
+
+test('Test maximum temperature',() =>{
+  const weekData = daysMaped(api.apiData, api.listOfDays);
+  const tempApi = Math.round(api.apiData.list[2].main.temp_min-273.15);
+  expect(weekData[0].maxTemp).toBe(tempApi)
+})
+
+test('Test description', ()=> {
+  const weekData = daysMaped(api.apiData, api.listOfDays);
+  expect(weekData[0].description).toBe(api.apiData.list[2].weather[0].description)
+})
